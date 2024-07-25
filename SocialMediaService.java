d596caf2197c689fc194ec443fde7a6f47c6d963
Tanay <98188627+Tanay-Agar@users.noreply.github.com>
2024-07-24 18:10:37 -0700

    Update SocialMediaService.java

--- SocialMediaService.java
+++ SocialMediaService.java
@@ -4,24 +4,41 @@ import biblioConnect_v3.User;
 import java.util.List;
 
 public interface SocialMediaService {
+    void createUserProfile(String username, String favoriteBooks, String readingHabits, String literaryPreferences);
+    void updateUserProfile(String username, String favoriteBooks, String readingHabits, String literaryPreferences);
+    UserProfile getUserProfile(String username);
+    void createPost(String username, String content);
+    void commentOnPost(String postId, String username, String content);
+    void likePost(String postId, String username);
+    void sharePost(String postId, String username);
+    void createGroup(String name, String description);
+    void joinGroup(String groupId, String username);
+    void leaveGroup(String groupId, String username);
+    void postInGroup(String groupId, String username, String content);
+    List<String> getGroupDiscussions(String groupId);
+    void followUser(String followerUsername, String followedUsername);
+    void unfollowUser(String followerUsername, String unfollowedUsername);
+    List<String> getFollowers(String username);
+    List<String> getFollowing(String username);
+    void createEvent(String name, String date, String description, String location);
+    void rsvpToEvent(int eventId, String username);
+    List<Event> viewUpcomingEvents();
     void showSocialMediaMenu(User currentUser);
-    void viewProfile(String username);
-    void editProfile(User user);
-    void postMessage(User user, String message);
-    List<String> viewRecentPosts();
-    void likePost(User user, int postId);
-    void joinGroup(User user, String groupName);
-    void leaveGroup(User user, String groupName);
-    void viewGroupDiscussions(String groupName);
-    void postToGroupDiscussion(User user, String groupName, String message);
-    void followUser(User follower, String followedUsername);
-    void unfollowUser(User follower, String unfollowedUsername);
-    List<String> viewFollowers(String username);
-    List<String> viewFollowing(String username);
-    List<String> viewUpcomingEvents();
-    void rsvpToEvent(User user, int eventId);
-    
-    static SocialMediaService getInstance() {
-        return SocialMediaServiceImpl.getInstance();
-    }
+    void handleProfileOperations(User currentUser);
+    void handleCreatePost(User currentUser);
+    void handleViewRecentPosts();
+    void handleCommentOnPost(User currentUser);
+    void handleLikePost(User currentUser);
+    void handleSharePost(User currentUser);
+    void handleCreateGroup(User currentUser);
+    void handleGroupOperations(User currentUser);
+    void handleViewGroupDiscussions();
+    void handleGroupChat(User currentUser);
+    void handleFollowOperations(User currentUser);
+    void handleViewFollowersFollowing(User currentUser);
+    void handleViewEvents();
+    void handleCreateEvent(User currentUser);
+    void handleRSVPToEvent(User currentUser);
+    List<Post> getRecentPosts(int limit);
+    List<String> listAllGroups();
 }
