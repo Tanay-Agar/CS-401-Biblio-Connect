bf50465c98e0e9449abe04ffcac1f59ade217a60
Tanay <98188627+Tanay-Agar@users.noreply.github.com>
2024-07-24 18:08:23 -0700

    Update Event.java

--- Event.java
+++ Event.java
@@ -7,30 +7,29 @@ public class Event {
     private int id;
     private String name;
     private String date;
+    private String description;
+    private String location;
     private List<String> attendees;
 
-    public Event(int id, String name, String date) {
+    public Event(int id, String name, String date, String description, String location) {
         this.id = id;
         this.name = name;
         this.date = date;
+        this.description = description;
+        this.location = location;
         this.attendees = new ArrayList<>();
     }
-
-    public int getId() {
-        return id;
+    
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public List<String> getAttendees() {
-        return attendees;
-    }
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDate() { return date; }
+    public String getDescription() { return description; }
+    public String getLocation() { return location; }
+    public List<String> getAttendees() { return attendees; }
 
     public void addAttendee(String username) {
         if (!attendees.contains(username)) {
@@ -48,6 +47,8 @@ public class Event {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", date='" + date + '\'' +
+                ", description='" + description + '\'' +
+                ", location='" + location + '\'' +
                 ", attendees=" + attendees.size() +
                 '}';
     }
